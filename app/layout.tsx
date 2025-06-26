c5eff7037298ec5ec6fe29adf13cd8505919cc05
User <user@example.com>
2025-06-26 13:30:05 +0800

    update init

--- app/layout.tsx
+++
@@ -1,8 +0,0 @@
-// The root layout is required and will redirect to the default locale
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
